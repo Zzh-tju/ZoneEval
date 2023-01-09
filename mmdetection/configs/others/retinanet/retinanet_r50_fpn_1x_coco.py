626c0a124a60771a8aa3c31ec34083daf86ecda0
Zhaohui Zheng <57825730+Zzh-tju@users.noreply.github.com>
2023-01-09 17:51:13 +0800

    Add files via upload

---
+++ mmdetection/configs/others/retinanet/retinanet_r50_fpn_1x_coco.py
@@ -0,0 +1,11 @@
+_base_ = [
+    '../_base_/models/retinanet_r50_fpn.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+model = dict(
+    test_cfg=dict(zone_eval=True))
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
