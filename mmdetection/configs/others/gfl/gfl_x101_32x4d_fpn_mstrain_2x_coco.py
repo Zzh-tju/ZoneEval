29d1a8ad687a170e248186bd68081d12d4d240dd
Zhaohui Zheng <57825730+Zzh-tju@users.noreply.github.com>
2023-01-14 02:24:33 +0800

    Update gfl_x101_32x4d_fpn_mstrain_2x_coco.py

--- mmdetection/configs/others/gfl/gfl_x101_32x4d_fpn_mstrain_2x_coco.py
+++ mmdetection/configs/others/gfl/gfl_x101_32x4d_fpn_mstrain_2x_coco.py
@@ -13,4 +13,5 @@ model = dict(
         norm_eval=True,
         style='pytorch',
         init_cfg=dict(
-            type='Pretrained', checkpoint='open-mmlab://resnext101_32x4d')))
+            type='Pretrained', checkpoint='open-mmlab://resnext101_32x4d')),
+    test_cfg=dict(zone_eval=True))
