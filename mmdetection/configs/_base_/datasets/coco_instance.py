0b3f0bb4feb1d489e0cef9e6b66b08ce863abb9e
Zhaohui Zheng <57825730+Zzh-tju@users.noreply.github.com>
2023-01-14 02:18:27 +0800

    Update coco_instance.py

--- mmdetection/configs/_base_/datasets/coco_instance.py
+++ mmdetection/configs/_base_/datasets/coco_instance.py
@@ -46,4 +46,4 @@ data = dict(
         ann_file=data_root + 'annotations/instances_val2017.json',
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
-evaluation = dict(metric=['bbox'])
+evaluation = dict(metric=['bbox', 'segm'])
