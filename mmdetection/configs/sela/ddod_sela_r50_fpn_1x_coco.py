2f329974f5475d2a596822d7a7cadae3b99daf30
Zhaohui Zheng <57825730+Zzh-tju@users.noreply.github.com>
2023-01-18 09:33:31 +0800

    Update ddod_sela_r50_fpn_1x_coco.py

--- mmdetection/configs/sela/ddod_sela_r50_fpn_1x_coco.py
+++ mmdetection/configs/sela/ddod_sela_r50_fpn_1x_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/datasets/coco_detection.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    '../others/_base_/datasets/coco_detection.py',
+    '../others/_base_/schedules/schedule_1x.py', '../others/_base_/default_runtime.py'
 ]
 
 model = dict(
